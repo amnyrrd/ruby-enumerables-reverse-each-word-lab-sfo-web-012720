11d3258ad0486cfd5c4f70dab6ecaaec9ecc3577
Aaron Minyard <amnyrrd@gmail.com>
2020-01-19 01:53:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,5 +6,8 @@ def reverse_each_word(str)
 end
 
 def reverse_each_word(str)
-  
+  arr = str.split(' ')
+  reversed = []
+  arr.collect { |word| reversed << word.reverse }
+  reversed.join(' ')
 end
